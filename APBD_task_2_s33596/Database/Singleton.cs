8870b381c5f975a28adc39eaf640b59b7b4de555
s33596 <s33596@pjwstk.edu.pl>
2026-03-17 16:13:04 +0100

    init comit

---
+++ APBD_task_2_s33596/Database/Singleton.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APBD_T2_s33596.Database
+{
+    public class Singleton
+    {
+        private static Singleton? _instance;
+        public static Singleton Instance
+        {
+            get
+            {
+                _instance ??= new Singleton();
+                return _instance;
+            }
+        }
+
+        private Singleton() { }
+
+        //TODO: add collections for items in the exercise
+        //public List<Class> Class { get; } = new();
+    }
+}
